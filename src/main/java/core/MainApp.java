f1de3df3a6029717fbab1b1182c602b1a517d68e
whereisalemon-lab <whereisalemon.gmail.com>
2026-02-24 20:20:16 +0100

    Add Machine OOP implementation

--- src/main/java/core/MainApp.java
+++ src/main/java/core/MainApp.java
@@ -1,50 +1,50 @@
 
-class Machine(){
+public abstract class Machine {
     public abstract void doWork();
     public abstract void stopWork();
 }
-class Truck extends Machine{
+class Truck extends Machine {
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("A Truck started work");
     }
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("A Truck stopped work");
     }
 
 }
-class Bulldozer extends Machine{
+class Bulldozer extends Machine {
     @Override
-    public void doWork(){
+    public void doWork() {
         System.out.println("A Bulldozer started work");
     }
     @Override
-    public void stopWork(){
+    public void stopWork() {
         System.out.println("A Bulldozer stopped work");
     }
 
 }
-class Excavator extends Machine{
+class Excavator extends Machine {
     @Override
-    public void doWork(){
-        System.out.println("A Excavator started work");
+    public void doWork() {
+        System.out.println("An Excavator started work");
     }
     @Override
-    ublic void stopWork(){
-        System.out.println("A Excavator stopped work");
+    public void stopWork() {
+        System.out.println("An Excavator stopped work");
     }
 
 }
 
-public class MainApp{
-    public static void main(String[] args){
-        Machine[] machines={
+public class MainApp {
+    public static void main(String[] args) {
+        Machine[] machines = {
             new Truck(),
-            new Buldozer(), 
+            new Bulldozer(), 
             new Excavator()
         };
-        for(Machine machine:machines){
+        for(Machine machine : machines) {
             machine.doWork();
             machine.stopWork();
         }
